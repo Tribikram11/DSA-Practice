f86202eaaff38c0ff079a51b72e5ee622e31b988
bikram <22btaml33@suiit.ac.in>
2026-08-19 23:41:49 +0530

    add function to check for nearby duplicates in an array

---
+++ test-1/3.js
@@ -0,0 +1,20 @@
+function containsNearbyDuplicate(nums, k) {
+    const seen = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        
+        if (seen.has(num) && i - seen.get(num) <= k) {
+            return true;
+        }
+        
+        seen.set(num, i);
+    }
+    
+    return false;
+}
+
+
+console.log(containsNearbyDuplicate([1, 2, 3, 1], 3));
+console.log(containsNearbyDuplicate([1, 0, 1, 1], 1)); 
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2)); 
