8a021a63aae25e15ff265584ee85d9660bd5381f
bikram <22btaml33@suiit.ac.in>
2026-08-25 22:49:47 +0530

    add function to find all unique triplets in an array that sum to zero

---
+++ Two-pointer/third.js
@@ -0,0 +1,43 @@
+// 3sum
+
+function threeSum(arr) {
+    arr.sort((a, b) => a - b)
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i > 0 && arr[i] === arr[i - 1]) {
+            continue
+        }
+        let left = i + 1;
+        let right = arr.length - 1;
+
+        while (left < right) {
+
+            let sum = arr[i] + arr[left] + arr[right];
+
+            if (sum === 0) {
+                result.push([arr[i], arr[left], arr[right]])
+
+                left++;
+                right--;
+                if (left < right && arr[left] === arr[left - 1]) {
+                    left++;
+                }
+
+                if (left < right && arr[right] === arr[right + 1]) {
+                    right--;
+                }
+
+                
+            } else if (sum < 0) {
+                left++
+            } else {
+                right--;
+            }
+        }
+    }
+    return result
+
+}
+
+console.log(threeSum([-2, -1, -1, -1, -1, 0, 0, 0, 1, 2, 2, 5, 5, 7, 9]))
